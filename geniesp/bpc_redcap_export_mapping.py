613e0d2e570edd60efbfc59ec3504afb59102868
rxu17 <26471741+rxu17@users.noreply.github.com>
2024-11-14 15:11:29 -0800

    add comments

--- geniesp/bpc_redcap_export_mapping.py
+++ geniesp/bpc_redcap_export_mapping.py
@@ -521,6 +521,7 @@ class BpcProjectRunner(metaclass=ABCMeta):
     _SPONSORED_PROJECT = ""
     # Redcap codes to cbioportal mapping synid and form key is in
     # version 38, 42 were last stable version(s)
+    # NOTE: Should be pointed towards latest version of table
     _REDCAP_TO_CBIOMAPPING_SYNID = "syn25712693.53"
     # Run `git rev-parse HEAD` in Genie_processing directory to obtain shadigest
     _GITHUB_REPO = None
@@ -532,8 +533,10 @@ class BpcProjectRunner(metaclass=ABCMeta):
         "production":"syn21446571",
         "staging": "syn64018293"
     }
-    # main GENIE release folder (15.4-consortium)
+    # main GENIE release folder
     # Must use consortium release, because SEQ_YEAR is used
+    # NOTE: Must match release tracking sheet and release table info
+    # for the given cohort
     _MG_RELEASE_SYNID = "syn63602196"
     # PRISSMM documentation table
     _PRISSMM_SYNID = "syn22684834"
