56bfc2871d755aa936c0a397a490936739f57259
rxu17 <26471741+rxu17@users.noreply.github.com>
2024-11-16 00:58:58 -0800

    add to tests

--- tests/test_bpc_redcap_export_mapping.py
+++ tests/test_bpc_redcap_export_mapping.py
@@ -1,5 +1,5 @@
 import pytest
-from pytest import mock
+from unittest import mock
 
 import pandas as pd
 import synapseclient
@@ -16,10 +16,14 @@ def test_that_get_drug_variable_names_gets_expected_list():
     var_names = bpc_export.get_drug_variable_names()
     assert var_names == [
         "drugs_drug_1",
-        "drugs_drug_oth1" "drugs_drug_2",
-        "drugs_drug_oth2" "drugs_drug_3",
-        "drugs_drug_oth3" "drugs_drug_4",
-        "drugs_drug_oth4" "drugs_drug_5",
+        "drugs_drug_oth1",
+        "drugs_drug_2",
+        "drugs_drug_oth2",
+        "drugs_drug_3",
+        "drugs_drug_oth3",
+        "drugs_drug_4",
+        "drugs_drug_oth4",
+        "drugs_drug_5",
         "drugs_drug_oth5",
     ]
 
@@ -45,39 +49,76 @@ def test_get_mapping_data_calls_dd_if_use_grs_is_false(mock_syn):
 
 
 @pytest.mark.parametrize(
-    "input_mapping, var_names, output_mapping"[
+    "input_mapping, var_names, output_mapping",
+    [
         (
             pd.DataFrame(
                 {
-                    "Variable / Field Name": "drugs_drug_1",
-                    "Choices, Calculations, OR Slider Labels": "D001, Aspirin | D002, Ibuprofen | D003, Paracetamol",
+                    "Variable / Field Name": ["drugs_drug_1", "drugs_drug_2"],
+                    "Choices, Calculations, OR Slider Labels": [
+                        "D001, Aspirin | D002, Ibuprofen | D003, Paracetamol",
+                        "D004, Tylenol |",
+                    ],
                 }
             ),
-            ["drugs_drug_1"],
-            {"D001": "Aspirin", "D002": "Ibuprofen", "D003": "Paracetamol"},
+            ["drugs_drug_1", "drugs_drug_2"],
+            {
+                "Aspirin": "D001",
+                "Ibuprofen": "D002",
+                "Paracetamol": "D003",
+                "Tylenol": "D004",
+            },
         ),
         (
             pd.DataFrame(
                 {
-                    "variable": "drugs_drug_1",
-                    "labels": "D001, Aspirin | D002, Ibuprofen | D003, Paracetamol",
+                    "variable": ["drugs_drug_1"],
+                    "labels": ["D001, Aspirin | D002, Ibuprofen | D003, Paracetamol"],
                 }
             ),
             ["drugs_drug_1"],
-            {"D001": "Aspirin", "D002": "Ibuprofen", "D003": "Paracetamol"},
+            {"Aspirin": "D001", "Ibuprofen": "D002", "Paracetamol": "D003"},
         ),
         (
             pd.DataFrame(
                 {
-                    "variable": "ethnicity",
-                    "labels": "1",
+                    "variable": ["drugs_drug_1"],
+                    "labels": ["D001, Aspirin|"],
+                }
+            ),
+            ["drugs_drug_1"],
+            {"Aspirin": "D001"},
+        ),
+        (
+            pd.DataFrame(
+                {
+                    "variable": ["ethnicity"],
+                    "labels": ["1"],
                 }
             ),
             ["drugs_drug_1"],
             {},
         ),
+        (
+            pd.DataFrame(
+                {
+                    "variable": ["drugs_drug_1"],
+                    "labels": [
+                        "D001, Aspirin (alternative) | D002, Ibuprofen | D003, Paracetamol"
+                    ],
+                }
+            ),
+            ["drugs_drug_1"],
+            {"Aspirin": "D001", "Ibuprofen": "D002", "Paracetamol": "D003"},
+        ),
     ],
-    ids=["", "nothing to parse", ""],
+    ids=[
+        "multiple_drug_vars",
+        "diff_var_names",
+        "empty_split",
+        "nothing_to_parse",
+        "parenthesis_split",
+    ],
 )
 def test_that_parse_drug_mappings(input_mapping, var_names, output_mapping):
     result = bpc_export.parse_drug_mappings(mapping=input_mapping, var_names=var_names)
